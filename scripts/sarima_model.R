524bf952991545cf8d9021f0123b9029c72cc574
Rohit Kavuluru <91348918+rohitkav@users.noreply.github.com>
2023-12-11 19:43:03 -0800

    Update sarima_model.R

--- scripts/sarima_model.R
+++ scripts/sarima_model.R
@@ -6,9 +6,28 @@ library(lubridate)
 
 # Load data
 load("data/data_clean.RData")
-df <- data_clean
 
-str(df)
+dates <- data_clean$Date
+
+data <- data.matrix(data_clean[,-1])
+# Standardize data --> center around mean for each column
+mean <- apply(data, 2, mean)
+std <- apply(data, 2, sd)
+data <- scale(data, center = mean, scale = std)
+
+# Normalize, create func. --> make between 0 and 1 for activation function 
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+max <- apply(data, 2, max)
+min <- apply(data, 2, min)
+
+# Normalize data & get rid of adjusted close 
+data_normalized <- apply(data, 2, normalize)
+
+#Combine all datasets
+df <- as.data.frame(data_normalized)
+df <- cbind(Date = dates, df)
 
 # Convert 'date' to Date class and set it as the time index
 ts_data <- ts(df$Close, frequency = 1, start = c(year(df$Date[1]), month(df$Date[1])))
@@ -24,14 +43,11 @@ for (i in (window_size + 1):length(ts_data)) {
   # Extract the current window
   current_window <- ts_data[(i - window_size):(i - 1)]
   
-  # ARIMA Model (Non-Seasonal)
-  arima_model <- auto.arima(current_window)
-  
   # SARIMA Model (Seasonal)
   sarima_model <- auto.arima(current_window, seasonal = TRUE)
   
   # Forecast the next day
-  arima_forecast <- forecast(arima_model, h = 1)
+ 
   sarima_forecast <- forecast(sarima_model, h = 1)
   
   # Store the results in the forecasts dataframe
@@ -41,11 +57,6 @@ for (i in (window_size + 1):length(ts_data)) {
                                            SARIMA = sarima_forecast$mean[1]))  # Extract the first forecast value
   }
 
-# Visualize forecasts for ARIMA
-plot(ts_data, type = "l", col = "blue", lwd = 2, main = "ARIMA Model Forecast", xlab = "Date", ylab = "Closing Prices")
-lines(forecasts$Date, forecasts$Actual, col = "black", lwd = 2, lty = 2, type = "b", pch = 16)
-lines(forecasts$Date, forecasts$ARIMA, col = "red", lwd = 2, type = "b", pch = 16)
-legend("topright", legend = c("Actual", "ARIMA Forecast"), col = c("black", "red"), lwd = 2, pch = 16)
 
 #Visualize forecasts for SARIMA
 plot(ts_data, type = "l", col = "blue", lwd = 2, main = "SARIMA Model Forecast", xlab = "Date", ylab = "Closing Prices")
@@ -53,13 +64,39 @@ lines(forecasts$Date, forecasts$Actual, col = "black", lwd = 2, lty = 2, type = 
 lines(forecasts$Date, forecasts$SARIMA, col = "green", lwd = 2, type = "b", pch = 16)
 legend("topright", legend = c("Actual", "SARIMA Forecast"), col = c("black","green"), lwd = 2, pch = 16)
 
+actual_values <- forecasts$Actual
+sarima_forecast_values <- forecasts$SARIMA
 
+# Remove missing values, if any
+actual_values <- actual_values[!is.na(sarima_forecast_values)]
+sarima_forecast_values <- sarima_forecast_values[!is.na(sarima_forecast_values)]
 
+# Calculate squared errors
+squared_errors <- (sarima_forecast_values - actual_values)^2
 
+# Calculate mean squared error
+mse <- mean(squared_errors)
+
+# Calculate root mean squared error
+rmse <- sqrt(mse)
+
+# Print RMSE value
+cat("RMSE for SARIMA model:", rmse, "\n")
 
 
 ####### OLD MODELS
 
+# # ARIMA Model (Non-Seasonal)
+# arima_model <- auto.arima(current_window)
+
+
+#arima_forecast <- forecast(arima_model, h = 1)
+
+# # Visualize forecasts for ARIMA
+# plot(ts_data, type = "l", col = "blue", lwd = 2, main = "ARIMA Model Forecast", xlab = "Date", ylab = "Closing Prices")
+# lines(forecasts$Date, forecasts$Actual, col = "black", lwd = 2, lty = 2, type = "b", pch = 16)
+# lines(forecasts$Date, forecasts$ARIMA, col = "red", lwd = 2, type = "b", pch = 16)
+# legend("topright", legend = c("Actual", "ARIMA Forecast"), col = c("black", "red"), lwd = 2, pch = 16)
 
 # # Create lag features
 # lags <- 5:10
